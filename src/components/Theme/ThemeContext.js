25f3b3bc5182a5bde151fb2b7593ebf6cf2b0c56
Mambans <Perssons1996@gmail.com>
2020-06-21 18:36:25 +0200

    Changed both themes background colors abit.

--- src/components/Theme/ThemeContext.js
+++ src/components/Theme/ThemeContext.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const themes = {
   light : {
     name        : 'Light',
-    background  : 'rgb(230,230,230)',
+    background  : 'rgb(250,250,250)',
     color       : 'black',
     color2      : 'rgb(50,50,50)',
     color3      : 'rgb(125,125,125)',
@@ -11,7 +11,7 @@ export const themes = {
   },
   dark  : {
     name        : 'Dark',
-    background  : 'rgb(35, 35, 50)',
+    background  : 'rgb(35, 35, 45)',
     color       : '#ffffff',
     color2      : 'rgb(200,200,200)',
     color3      : 'rgb(175,175,175)',
